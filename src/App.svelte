fcc5e0d7f912f98c5d8e06b4e4d0e6b706116e21
mallowigi <heliosaian@gmail.com>
2019-06-26 22:45:05 +0300

    some animations

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@ import Panel from './components/Panel.svelte';
 import * as api from './api';
 import {app} from './store';
 import {storage} from './storage';
+import {fly} from 'svelte/transition'
 
   onMount(async _ => {
     $app.loading = true;
@@ -30,10 +31,10 @@ import {storage} from './storage';
 
 <main style="width: 100vw;height: 100vh;position: relative;">
 {#if $app.loading}
-  <div class="container">
+  <div class="container" transition:fly="{{ y: 200, duration: 2000 }}">
     <h4>Loading...</h4>
   </div>
 {:else}
-  <Panel></Panel>
+  <Panel transition:fly="{{ y: 200, duration: 2000 }}"></Panel>
 {/if}
 </main>
