2282b4d2509aec383664b7f37fb1b8ada165b1a3
Elior <eliorboukhobza@Eliors-MacBook-Pro.local>
2019-09-02 11:51:44 +0300

    gulp takss

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const copy = require('gulp-copy');
+const zip = require('gulp-zip');
+const clean = require('gulp-clean');
 
 const paths = {
-    styles: {
-        src: 'new/**/*.scss',
-        dest: 'dist'
-    }
+  styles: {
+    src: 'new/**/*.scss',
+    dest: 'dist',
+  },
 };
 
 gulp.task('styles', () => {
@@ -26,3 +29,21 @@ gulp.task('debugStyles', () => {
 gulp.task('watchStyles', () => {
     gulp.watch(paths.styles.src, gulp.parallel('styles'));
 });
+
+gulp.task('clean', () => {
+  return gulp.src(['release', 'dist'], {read: false, allowEmpty: true})
+    .pipe(clean());
+});
+
+gulp.task('copy', () => {
+  return gulp.src(['*.*', '!release.zip', 'dist/*.min.css', 'public/**/*'], {allowEmpty: true})
+    .pipe(copy('release'));
+});
+
+gulp.task('zip', () => {
+  return gulp.src('release')
+    .pipe(zip('release.zip'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('release', gulp.series('clean', 'styles', 'copy', 'zip'));
